6fa4c6420ba25e7ee4d5bcbd5f088a82a575b10d
Bill W <42258241+wwunsch23@users.noreply.github.com>
2018-12-12 18:23:18 -0600

    Added JAWS db

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,26 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "appTest",
-  password: "appTest01",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "appTest",
+    password: "appTest01",
+    database: "burgers_db"
+  });
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+  // Make connection.
+  connection.connect(function (err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
 
-// Export connection for our ORM to use.
-module.exports = connection;
+  // Export connection for our ORM to use.
+  module.exports = connection;
